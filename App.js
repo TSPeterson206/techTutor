f87c907ea6f54ecf9daa3ae59814754daba83318
TOBY PETERSON <tspeterson206@gmail.com>
2019-05-08 21:57:05 -0700

    added thunk and logger

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import AppNavigator from './AppNavigator';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <AppNavigator/>
-    );
-  }
+import { store } from './store'
+
+import React from 'react'
+
+import HomeScreen from './screens/HomeScreen'
+import LandingPage from './screens/LandingPage'
+import LinksScreen from './screens/LinksScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import Tutors from './screens/Tutors'
+
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+  Landing: LandingPage,
+  Links: LinksScreen,
+  Settings: SettingsScreen,
+  Tutors: Tutors,
+}, 
+{
+  initialRouteName: 'SignIn',
+  headerMode: 'none'
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+function App(){
+ return (
+    <Provider store={store}>
+      <AppContainer/>
+    </Provider>  
+  )
 }
+
+export default App;
